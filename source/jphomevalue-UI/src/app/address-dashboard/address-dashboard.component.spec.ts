77ed095f67522cd85f18932305244f91795b35a9
sid-ak <sidharthak25@gmail.com>
2022-03-21 20:16:31 -0400

    C1-30: Created two Address and Neighborhood parent components.

---
+++ source/jphomevalue-UI/src/app/address-dashboard/address-dashboard.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddressDashboardComponent } from './address-dashboard.component';
+
+describe('AddressDashboardComponent', () => {
+  let component: AddressDashboardComponent;
+  let fixture: ComponentFixture<AddressDashboardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AddressDashboardComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddressDashboardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ source/jphomevalue-UI/src/app/address-dashboard/address-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-address-dashboard',
+  templateUrl: './address-dashboard.component.html',
+  styleUrls: ['./address-dashboard.component.scss']
+})
+export class AddressDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
