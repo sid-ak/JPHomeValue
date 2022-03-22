6da7882167d7053dae1ffc09546523e6b7d81a46
sid-ak <sidharthak25@gmail.com>
2022-03-22 04:35:20 -0400

    CD-32: Module imports.

--- source/jphomevalue-UI/src/app/app.module.ts
+++ source/jphomevalue-UI/src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NeighborhoodDashboardComponent } from './neighborhood-dashboard/neighborhood-dashboard.component';
 import { AddressDashboardComponent } from './address-dashboard/address-dashboard.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NeighborhoodFilterComponent } from './neighborhood-filter/neighborhood-filter.component';
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatSelectModule } from '@angular/material/select'
+import { MatInputModule } from '@angular/material/input'
 
 @NgModule({
   declarations: [
     AppComponent,
     NeighborhoodDashboardComponent,
-    AddressDashboardComponent
+    AddressDashboardComponent,
+    NeighborhoodFilterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
