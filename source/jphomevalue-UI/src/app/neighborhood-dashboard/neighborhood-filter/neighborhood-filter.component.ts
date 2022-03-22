ffa361406de7ca4f4c27c591f052f4d7cab85c99
sid-ak <sidharthak25@gmail.com>
2022-03-22 05:26:58 -0400

    CD-32: Get neighborhood enum from string.

--- source/jphomevalue-UI/src/app/neighborhood-dashboard/neighborhood-filter/neighborhood-filter.component.ts
+++ source/jphomevalue-UI/src/app/neighborhood-dashboard/neighborhood-filter/neighborhood-filter.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, retry } from 'rxjs';
 import { NeighborhoodEnum } from '../../enums/neighborhood-enum';
 import { NeighborhoodFilterViewModel } from '../../view-models/neighborhood-filter-view-model';
 
@@ -41,12 +41,21 @@ export class NeighborhoodFilterComponent implements OnInit {
   private constructNeighborhoodVm(neighborhoodFilter: FormGroup): NeighborhoodFilterViewModel {
     if (neighborhoodFilter === (null || undefined)) return new NeighborhoodFilterViewModel();
     
-    this.neighborhoodVm.neighborhood = neighborhoodFilter.get('neighborhood')?.value 
+    this.neighborhoodVm.neighborhood = this.getNeighborhoodEnum(neighborhoodFilter.get('neighborhood')?.value)
       ?? NeighborhoodEnum.None;
     this.neighborhoodVm.timeframe = neighborhoodFilter.get('timeframe')?.value ?? -1;
     this.neighborhoodVm.walkScore = neighborhoodFilter.get('walkScore')?.value ?? -1;
     this.neighborhoodVm.transitScore = neighborhoodFilter.get('transitScore')?.value ?? -1;
     this.neighborhoodVm.bikeScore = neighborhoodFilter.get('bikeScore')?.value ?? -1;    
     return this.neighborhoodVm;
+  }
+
+  private getNeighborhoodEnum(neighborhood: string): NeighborhoodEnum {
+    switch (neighborhood) {
+      case "Tampa": return NeighborhoodEnum.Tampa;
+      case "StPetersburg": return NeighborhoodEnum.StPetersburg;
+      case "Clearwater": return NeighborhoodEnum.Clearwater;
+      default: return NeighborhoodEnum.None;
+    }
   }
 }
