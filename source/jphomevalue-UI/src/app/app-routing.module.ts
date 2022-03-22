8f30cd66122911361cc3a5adf4d7bd1f52e22d0e
sid-ak <sidharthak25@gmail.com>
2022-03-21 20:16:56 -0400

    CD-30: Set up routing to the newly created components.

--- source/jphomevalue-UI/src/app/app-routing.module.ts
+++ source/jphomevalue-UI/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddressDashboardComponent } from './address-dashboard/address-dashboard.component';
+import { NeighborhoodDashboardComponent } from './neighborhood-dashboard/neighborhood-dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "neighborhood-dashboard",
+    pathMatch: "full",
+    component: NeighborhoodDashboardComponent
+  },
+  {
+    path: "address-dashboard",
+    pathMatch: "full",
+    component: AddressDashboardComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
