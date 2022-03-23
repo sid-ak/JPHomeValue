6ad785b100004a9c2df1b6310040540295c8cef4
sid-ak <sidharthak25@gmail.com>
2022-03-23 16:51:52 -0400

    CD-41: Small responsive change that spiraled into a bigger one.

--- source/jphomevalue-UI/src/app/components/chart/chart.component.ts
+++ source/jphomevalue-UI/src/app/components/chart/chart.component.ts
@@ -2,16 +2,15 @@ import { Component } from '@angular/core';
 import { ChartOptions } from 'chart.js';
 
 @Component({
-  selector: 'app-forecast-model',
-  templateUrl: './forecast-model.component.html',
-  styleUrls: ['./forecast-model.component.scss']
+  selector: 'app-chart',
+  templateUrl: './chart.component.html',
+  styleUrls: ['./chart.component.scss']
 })
 export class ChartComponent {
-  data = {
+  readonly data = {
     datasets: [{
       label: 'My First Dataset',
       data: [65, 59, 80, 81, 56, 55, 40],
-      fill: false,
       borderColor: 'rgb(75, 192, 192)',
       tension: 0.1
     }]
