644a819fafa2326703e746977b44d79c55a911ba
sid-ak <sidharthak25@gmail.com>
2022-04-05 02:18:53 -0400

    CD-46: Update map based on city/address.

--- UI/src/app/components/map/map.component.ts
+++ UI/src/app/components/map/map.component.ts
@@ -1,18 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit } from '@angular/core';
+import { takeUntil } from 'rxjs';
+import { MapHelper } from 'src/app/helpers/map-helper';
 import { AddressService } from 'src/app/services/address-service';
+import { CityService } from 'src/app/services/city-service';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements OnInit, OnDestroy {
 
-  constructor (private readonly addressService: AddressService) { }
+  private readonly destroyed$ = new EventEmitter<boolean>(false);
+
+  constructor (
+    private readonly addressService: AddressService,
+    private readonly cityService: CityService) { }
 
   ngOnInit(): void {
-    this.addressService.addressChanged$.subscribe(
-      e => console.log(e)
+    this.cityService.cityChanged$.pipe(
+      takeUntil(this.destroyed$)).subscribe(
+      e => MapHelper.addMapScript(e)
     );
+    
+    this.addressService.addressFilterChanged$.pipe(
+      takeUntil(this.destroyed$)).subscribe(
+        e => MapHelper.addMapScript(undefined, e)
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next(true);
   }
 }
--- UI/src/app/helpers/map-helper.ts
+++ UI/src/app/helpers/map-helper.ts
@@ -1,4 +1,8 @@
+import { AddressInfo } from '../common/address-data';
 import { Constants } from '../constants';
+import { CityEnum } from '../enums/city-enum';
+import { AddressFilterModel } from '../models/address-filter-model';
+import { FirebaseDbService } from '../services/firebase-db.service';
 
 export class MapHelper {
   private static readonly mapElement: HTMLElement | null
@@ -10,27 +14,60 @@ export class MapHelper {
    * Hacky.
    * Credit: https://stackoverflow.com/a/42733315
    */
-  public static addMapScript(): void {
-    if(!MapHelper.mapElement) {
-      document.body.appendChild(Object.assign(
-        document.createElement('script'), {
-          type: 'text/javascript',
-          src: Constants.googleMapsUrl,
-          onload: () => MapHelper.initializeMap()
-        })).remove() // Bad way to have the map re-render. TODO: Need to use an Observable wrapped around this.
-    }
-    return;
+  public static addMapScript(
+    city: CityEnum = CityEnum.None, 
+    addressFilter: AddressFilterModel = new AddressFilterModel()): void {
+
+      if(!MapHelper.mapElement) {
+        document.body.appendChild(Object.assign(
+          document.createElement('script'), {
+            type: 'text/javascript',
+            src: Constants.googleMapsUrl,
+            onload: () => this.initializeMap(city, addressFilter)
+          })).remove() // Bad way to have the map re-render. TODO: Need to use an Observable wrapped around this.
+      }
+
+      return;
   }
 
   /**
-   * Initializes the map by accessing the DOM.
+   * Initializes the map with the city or lat/lng by accessing the DOM.
    */
-  private static initializeMap(): void {
-    let map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
-      center: {lat: 27.964157, lng: -82.452606}, // Tampa, FL lat and lng.
-      zoom: 10,
-      streetViewControl: false
-    });
-    console.log(map); // Do something with this.
+  private static initializeMap(city: CityEnum, addressFilter: AddressFilterModel): void {
+    if (addressFilter.lat === 0 && addressFilter.lat === 0) {
+      new google.maps.Map(document.getElementById('map') as HTMLElement, {
+        center: this.getLatLngFromCity(city),
+        zoom: 12,
+        streetViewControl: false
+      });
+    } else {
+      new google.maps.Map(document.getElementById('map') as HTMLElement, {
+        center: this.getLatLngFromAddressFilter(addressFilter),
+        zoom: 18,
+        streetViewControl: false
+      });
+    }
+  }
+
+  /**
+   * Gets lat and long from an AddressFilterModel.
+   * @param addressFilter 
+   * @returns a lat/long of type google.maps.LatLng
+   */
+  private static getLatLngFromAddressFilter(addressFilter: AddressFilterModel): google.maps.LatLng {
+    return new google.maps.LatLng(addressFilter.lat!, addressFilter.lng!)
+  }
+
+  /**
+   * Gets lat and long from the city, returns Tampa's lat and long if no match is found.
+   * @param city 
+   * @returns 
+   */
+  private static getLatLngFromCity(city: CityEnum): google.maps.LatLng {
+    return new Map<CityEnum, google.maps.LatLng>([
+      [CityEnum.Tampa, new google.maps.LatLng(Constants.tampaLatLng[0], Constants.tampaLatLng[1])],
+      [CityEnum.StPetersburg, new google.maps.LatLng(Constants.stPeteLatLng[0], Constants.stPeteLatLng[1])],
+      [CityEnum.Clearwater, new google.maps.LatLng(Constants.clearwaterLatLng[0], Constants.clearwaterLatLng[1])]
+    ]).get(city) ?? new google.maps.LatLng(Constants.tampaLatLng[0], Constants.tampaLatLng[1]);
   }
 }
